40a4a70d7fed206d4d20c82088e035d9cc76f115
MiguiGH01 <105802045+MiguiGH01@users.noreply.github.com>
2023-04-09 13:22:28 +0200

    Delete backend directory

--- backend/src/main/java/es/upm/dit/isst/G07rh/repository/TrabajadorRepository.java
+++
@@ -1,8 +0,0 @@
-package es.upm.dit.isst.G07rh.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import es.upm.dit.isst.G07rh.model.*;
-
-public interface TrabajadorRepository extends CrudRepository<TRABAJADOR, String> {
-}
