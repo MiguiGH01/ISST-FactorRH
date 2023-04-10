c1c726ef066a2425d16bbaf32dd4d1901b3c0b9b
MiguiGH01 <105802045+MiguiGH01@users.noreply.github.com>
2023-04-10 22:07:30 +0200

    Add files via upload

--- backend/src/main/java/es/upm/dit/isst/G07rh/G07rhApplication.java
+++ backend/src/main/java/es/upm/dit/isst/G07rh/G07rhApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import es.upm.dit.isst.G07rh.model.TRABAJADOR;
-import es.upm.dit.isst.G07rh.repository.TrabajadorRepository;
+
+
+import es.upm.dit.isst.G07rh.model.*;
+import es.upm.dit.isst.G07rh.repository.*;
 
 
 
@@ -22,12 +24,25 @@ public class G07rhApplication {
 		return (args) -> {
 			trabajadorRepository.save(new TRABAJADOR(
 				"admin@gmail.com", 0, "Admin", 0, 0, 0, true,0,0,15,12));
-				trabajadorRepository.save(new TRABAJADOR(
+			trabajadorRepository.save(new TRABAJADOR(
 				"trabajador1@gmail.com", 0, "Trabajador1", 0, 0, 0, false,0,0,15,12));	
-				trabajadorRepository.save(new TRABAJADOR(
+			trabajadorRepository.save(new TRABAJADOR(
 				"trabajador2@gmail.com", 0, "Trabajador2", 0, 0, 0, false,0,0,15,12));	
-					trabajadorRepository.save(new TRABAJADOR(
-				"usuario", 0, "1234", 0, 0, 0, true,0,0,15,12));		
+			trabajadorRepository.save(new TRABAJADOR(
+				"usuario@gmail.com", 0, "1234", 0, 0, 0, true,0,0,15,12));
+			trabajadorRepository.save(new TRABAJADOR(
+				"usuario2@gmail.com", 0, "2131", 0, 0, 0, true,0,0,15,12));
+
+						
+		};
+	}
+
+	@Bean 
+	public CommandLineRunner initialNotificacionData(NotificacionRepository notificacionRepository) {
+		return (args) ->{
+			notificacionRepository.save(new NOTIFICACION(
+					1,"sus muerrtos ","a caballo cojoones"
+			));
 		};
 	}
 
--- backend/src/main/java/es/upm/dit/isst/G07rh/repository/NotificacionRepository.java
+++ backend/src/main/java/es/upm/dit/isst/G07rh/repository/NotificacionRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import es.upm.dit.isst.G07rh.model.*;
 
-public interface TrabajadorRepository extends CrudRepository<TRABAJADOR, String> {
+public interface NotificacionRepository extends CrudRepository<NOTIFICACION, Long> {
      
 }
