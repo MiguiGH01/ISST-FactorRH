df3c4c23627cdc9e7b0575ae8ee3b6fb16f1d668
MiguiGH01 <105802045+MiguiGH01@users.noreply.github.com>
2023-05-01 22:42:24 +0200

    Add files via upload

--- frontend/src/components/Horarios/HorariosEmpleados.jsx
+++ frontend/src/components/Horarios/HorariosEmpleados.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useState } from 'react-router-dom';
 import Liner from "../Interfaces/Liner";
 import Card from 'react-bootstrap/Card';
 import { Container, Col, Row, Button } from "react-bootstrap";
@@ -9,9 +9,38 @@ const HorariosEmpleados = (props) => {
 
     const idHorarioNumero = parseInt(idHorario, 10);
 
-    const empleadoList = props.empleados2;
-
     const empleado = props.empleados2.find(empleado => empleado.id === idHorarioNumero);
+
+    function convertirFecha(fechaString) {
+        const partesFecha = fechaString.split('-');
+        const fecha = new Date(partesFecha[0], partesFecha[1] - 1, partesFecha[2]);
+        const opcionesFecha = { day: 'numeric', month: 'long', year: 'numeric' };
+        const fechaFormateada = fecha.toLocaleDateString('es-ES', opcionesFecha);
+        return fechaFormateada;
+    }
+
+    function convertirMinutosAHoras(minutos) {
+        if (minutos < 0) {
+            const horas = Math.floor(-minutos / 60);
+            const minutosRestantes = Math.floor(-minutos % 60);
+        
+            const tiempoFormateado = `-${horas.toString()}h ${minutosRestantes.toString().padStart(2, '0')}min`;
+            return tiempoFormateado;
+
+        } else {
+            const horas = Math.floor(minutos / 60);
+            const minutosRestantes = Math.floor(minutos % 60);
+        
+            const tiempoFormateado = `${horas.toString()}h ${minutosRestantes.toString().padStart(2, '0')}min`;
+            return tiempoFormateado;
+        }
+    }
+
+    function convertirFormatoHora(horaCompleta) {
+        const fecha = new Date(`1970-01-01T${horaCompleta}`);
+        return fecha.toLocaleTimeString('en-US', {hour12: false, hour: '2-digit', minute:'2-digit'});
+    }   
+      
 
     if (!empleado) {
         return <div class="contenedor-flexbox" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center", margin: "auto"}}>Empleado no encontrado</div>;
@@ -25,35 +54,46 @@ const HorariosEmpleados = (props) => {
         <Row style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center", margin: "auto"}}>
             <h2><b>{empleado.nombreCompleto}</b></h2> 
         </Row>
-        <Row style={{ display: 'flex', flexWrap: 'wrap', overflowY: "auto", overflowX: "hidden" }}>
-            <hr className="my-4"/>
+        <Row>
+            <div id="productosresultados" style={{ height: "68vh", overflowY: "auto", overflowX: "hidden" }}>
             {empleado.horarios.map((horario, index) => (
-            <div key={horario.id} style={{ flexBasis: '20%', padding: '1%'}}>
-                    <Card key={horario.id} >
-                        <Card.Body>
-                                <div class='row' style={{marginLeft:"4vw"}}>
-                                    <h4 style={{fontFamily:"fantasy"}}>{horario.fecha}</h4>
+                <Col  key={index}>
+                <div className="my-1">
+                    <Card>
+                    <Card.Body>
+                        <div class="row">
+                            <div className="d-flex justify-content-center align-items-center mb-2">
+                            <h3 id="catálogo"><b>{convertirFecha(horario.fecha)}</b></h3>
+                            </div>
+                                <hr className="my-2" />
+                                <div className="col-4">
+                                        <p style={{marginTop:"1.2vh"}}><b>Entrada: </b>{<input type='time' style={{width:76}} pattern="\d{2}:\d{2}" defaultValue={convertirFormatoHora(horario.horaEntrada)}/>}</p>
                                 </div>
-                                <div class="row">
-                                    <div class="col-8">
-                                    <p><b>Entrada:</b>  </p>
-                                    <p><b>Entrada Definida:</b></p>
-                                    <p><b>Salida:</b></p>
-                                    <p><b>Salida Definida:</b></p>
-                                    <p><b>Diferencia Tiempo:</b></p>
+                                <div className="col-4">
+                                        <p style={{marginTop:"1.2vh"}} ><b>Salida: </b>{<input type='time' style={{width:76}} pattern="\d{2}:\d{2}" defaultValue={convertirFormatoHora(horario.horaSalida)}/>}</p>
                                 </div>
-                                <div class="col-4">
-                                    <p>{horario.horaEntrada}</p>
-                                    <p>{horario.horaDefEntrada}</p>
-                                    <p>{horario.horaSalida}</p>
-                                    <p>{horario.horaDefSalida}</p>
-                                    <p>{horario.minutosTot}</p>
+                                <div className="col-4">
+                                    <button className="btn btn-primary btn-block" style={{marginTop:"0.8vh"}}>Registrar hora</button>
                                 </div>
                             </div>
-                        </Card.Body> 
+                        <hr className="my-2" style={{width:"90%", margin:"auto", border: "none", borderTop: "2px dashed black"}}/>
+                            <div class="row">
+                                <div className="col-4">
+                                        <p style={{marginTop:"1.2vh"}}><b>Tiempo Total: </b>{convertirMinutosAHoras(horario.minutosTot)}</p>
+                                </div>
+                                <div className="col-4">
+                                        <p style={{marginTop:"1.2vh"}} ><b>Tiempo Extra: </b>{convertirMinutosAHoras(horario.minutosExt)}</p>
+                                </div>
+                                <div className="col-4">
+                                        <p style={{marginTop:"1.2vh"}} ><b>Tiempo Pausado: </b>{convertirMinutosAHoras(horario.minutosPau)}</p>
+                                </div>
+                            </div>
+                    </Card.Body> 
                     </Card>
                 </div>
+                </Col>
             ))}
+            </div>
         </Row>
     </div>
     )
