05f1563d2d39d42d0780dac1b673c22a6c27b87c
sergirem <123004147+sergirem@users.noreply.github.com>
2023-05-03 08:14:23 +0200

    Add files via upload

--- frontend/src/components/Nominas/NominaEmpleados.jsx
+++ frontend/src/components/Nominas/NominaEmpleados.jsx
@@ -9,28 +9,157 @@ import { MDBContainer, MDBInput, MDBCheckbox, MDBBtn, MDBIcon, MDBRow, MDBCol, M
 
 const NominaEmpleados = (props) => {
     const { idNomina } = useParams(); // Accede a la ID desde props.match.params
-
-
-    const [id, setId] = useState('');
-    const [nombreCompleto, setNombreCompleto] = useState('');
-    const [numeroTelefono, setNumeroTelefono] = useState('');
-    const [correoElectronico, setCorreoElectronico] = useState('');
-    const [password, setPassword]= useState('');
-    const [rec, setRec] = useState(false);
-    const [departamento, setDepartamento] = useState('');
-    const [puesto, setPuesto] = useState('');
+    const [file, setFile] = useState('');
 
     const idNominaNumero = parseInt(idNomina, 10);
- 
-    const empleadoList = props.empleados2;
-
     const empleado = props.empleados2.find(empleado => empleado.id === idNominaNumero);
 
+
+    const handleCrearNomina = (event) => {
+        setFile(event.target.files[0]);
+      };
+    
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        const formData = new FormData();
+        formData.append('file', file);
+    
+        const nominaItem = {
+            file
+        }     
+          const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: formData,
+            ...nominaItem
+          };
+          const json = await requestOptions.json();
+          console.log(json);
+        
+        
+        await fetch('http://localhost:8080/empleadosv2', requestOptions);
+
+        window.location.href = '/home';
+        
+        setFile('');
+
+      };
+    
+  
     if (!empleado) {
         return <div class="contenedor-flexbox" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center", margin: "auto"}}>Empleado no encontrado</div>;
       }
 
-      const handleCrearNomina = async (e) => {
+    return (
+    <div class="contenedor-flexbox" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center", margin: "auto"}}>
+        <Row>
+            <Liner/>
+        </Row>
+        <Row style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center", margin: "auto"}}>
+            <h2><b>{empleado.nombreCompleto}</b></h2> 
+        </Row>
+        <Col md>
+                <div >
+                    <Row className="my-2">
+                        <Card className="flex-fill">
+                            <Card.Body>
+                                    <div class="row">
+                                        <h2>{empleado.nombreCompleto}</h2>
+                                        <hr className="my-4" />
+                                        <div class="col-3">
+                                            <p><b>Puesto:</b> {empleado.puesto}</p>
+                                        </div>
+                                        <div class="col-3">
+                                            <p><b>Departamento:</b> {empleado.departamento}</p>
+                                        </div>
+                                        <p><b>Descripción:</b> </p>
+                                        <Card.Text>
+                                        </Card.Text>
+                                    </div>
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                </div>
+            </Col>
+        <Row>
+
+        <form onSubmit={handleSubmit} >
+      <label >
+        Selecciona un archivo PDF:
+        <input type="file"/>
+      </label>
+      <button type="submit" onChange={handleCrearNomina} className="btn btn-primary btn-block"> Subir nómina</button>
+         </form>
+
+     </Row>
+
+     <div>
+     {empleado.nominas.map((empleadosItem) => (
+                    <Col md>
+                        <Row className="my-2">
+                            <Card className="flex-fill">
+                                <Card.Body>
+                                    <div class="row">
+                                            <hr className="my-4" />
+                                            <div class="col-6">
+                                                <p><b>Nominas de mes:</b> {empleadosItem.nominas}</p>
+                                            </div>
+                                        </div>
+                                </Card.Body> 
+                            </Card>
+                        </Row>
+                    </Col>   
+     ))};
+
+    </div>
+
+         
+    </div>
+    )
+
+}
+
+export default NominaEmpleados;
+
+
+
+/*
+  {empleado.rec ?  <button type="submit"> Subir nómina</button>: <div></div>}
+
+
+<Row style={{ display: 'flex', flexWrap: 'wrap', overflowY: "auto", overflowX: "hidden" }}>
+            <hr className="my-4"/>
+            {empleado.horarios.map((horario, index) => (
+            <div key={horario.id} style={{ flexBasis: '20%', padding: '1%'}}>
+                    <Card key={horario.id} >
+                        <Card.Body>
+                                <div class='row' style={{marginLeft:"4vw"}}>
+                                    <h4 style={{fontFamily:"fantasy"}}>{horario.fecha}</h4>
+                                </div>
+                                <div class="row">
+                                    <div class="col-8">
+                                    <p><b>Entrada:</b>  </p>
+                                    <p><b>Entrada Definida:</b></p>
+                                    <p><b>Salida:</b></p>
+                                    <p><b>Salida Definida:</b></p>
+                                    <p><b>Diferencia Tiempo:</b></p>
+                                </div>
+                                <div class="col-4">
+                                    <p>{horario.horaEntrada}</p>
+                                    <p>{horario.horaDefEntrada}</p>
+                                    <p>{horario.horaSalida}</p>
+                                    <p>{horario.horaDefSalida}</p>
+                                    <p>00:00:00</p>
+                                </div>
+                            </div>
+                        </Card.Body> 
+                    </Card>
+                </div>
+            ))}
+        </Row>
+        
+        const handleCrearNomina = async (e) => {
         e.preventDefault();
         if (nombreCompleto.trim() && numeroTelefono.trim() && correoElectronico.trim() && password.trim() && departamento.trim() && puesto.trim()) {
             
@@ -67,80 +196,13 @@ const NominaEmpleados = (props) => {
         }
     };
 
-    const handleCheckboxChange = (event) => {
-        setRec(event.target.checked); // Actualiza el estado con el valor de la casilla de verificación
-      }
 
-    return (
-    <div class="contenedor-flexbox" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center", margin: "auto"}}>
-        <Row>
-            <Liner/>
-        </Row>
-        <Row style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center", margin: "auto"}}>
-            <h2><b>{empleado.nombreCompleto}</b></h2> 
-        </Row>
-        <Col md>
-                <div >
-                    <Row className="my-2">
-                        <Card className="flex-fill">
-                            <Card.Body>
-                                    <div class="row">
-                                        <h2>{empleado.nombreCompleto}</h2>
-                                        <hr className="my-4" />
-                                        <div class="col-3">
-                                            <p><b>Puesto:</b> {empleado.puesto}</p>
-                                        </div>
-                                        <div class="col-3">
-                                            <p><b>Departamento:</b> {empleado.departamento}</p>
-                                        </div>
-                                        <p><b>Descripción:</b> </p>
-                                        <Card.Text>
-                                        </Card.Text>
-                                    </div>
-                            </Card.Body>
-                        </Card>
-                    </Row>
-                </div>
-            </Col>
-        <Row>
-            {empleado.rec ? <button type="submit" className="btn btn-primary" onClick={handleCrearNomina}>Subir nómina</button> : <div></div>}
 
-        </Row>
-         
-    </div>
-    )
 
-}
+      const idNominaNumero = parseInt(idNomina, 10);
+ 
+    const empleadoList = props.empleados2;
 
-export default NominaEmpleados;
+    const empleado = props.empleados2.find(empleado => empleado.id === idNominaNumero);
 
-/*<Row style={{ display: 'flex', flexWrap: 'wrap', overflowY: "auto", overflowX: "hidden" }}>
-            <hr className="my-4"/>
-            {empleado.horarios.map((horario, index) => (
-            <div key={horario.id} style={{ flexBasis: '20%', padding: '1%'}}>
-                    <Card key={horario.id} >
-                        <Card.Body>
-                                <div class='row' style={{marginLeft:"4vw"}}>
-                                    <h4 style={{fontFamily:"fantasy"}}>{horario.fecha}</h4>
-                                </div>
-                                <div class="row">
-                                    <div class="col-8">
-                                    <p><b>Entrada:</b>  </p>
-                                    <p><b>Entrada Definida:</b></p>
-                                    <p><b>Salida:</b></p>
-                                    <p><b>Salida Definida:</b></p>
-                                    <p><b>Diferencia Tiempo:</b></p>
-                                </div>
-                                <div class="col-4">
-                                    <p>{horario.horaEntrada}</p>
-                                    <p>{horario.horaDefEntrada}</p>
-                                    <p>{horario.horaSalida}</p>
-                                    <p>{horario.horaDefSalida}</p>
-                                    <p>00:00:00</p>
-                                </div>
-                            </div>
-                        </Card.Body> 
-                    </Card>
-                </div>
-            ))}
-        </Row>*/
+*/
